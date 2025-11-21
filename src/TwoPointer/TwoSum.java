806fb22e1b4ddf3dba61c0f47730e04c17884bc9
SaishWadnere <saishwadnere3@gmail.com>
2025-11-22 01:36:39 +0530

    Added Two Sum to Two pointer

--- src/TwoPointer/TwoSum.java
+++ src/TwoPointer/TwoSum.java
@@ -1,4 +1,22 @@
-package TwoPointer;
+/*
+Pattern: HashMap
+Problem: Two Sum (LeetCode 1)
+Difficulty: Easy
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+import java.util.HashMap;
 
 public class TwoSum {
+    public int[] Solution(int[] nums,int target){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i =0 ; i < nums.length ; i++){
+            int x = target - nums[i];
+            if(map.containsKey(x)){
+                return new int[]{map.get(x),i};
+            }
+            map.put(nums[i],i);
+        }
+        return  new int[]{-1,-1};
+    }
 }
