b6bd95a60d59489f2384fbeac98475aa0783d7bb
SaishWadnere <saishwadnere3@gmail.com>
2026-01-10 21:39:25 +0530

    Fixed the Remove Duplicates in sorted array solution

--- src/TwoPointer/RemoveDuplicates.java
+++ src/TwoPointer/RemoveDuplicates.java
@@ -11,7 +11,7 @@ public class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
         int j =1;
         for( int i = 1 ; i < nums.length ; i++){
-            if(nums[i] != nums[j]){
+            if(nums[i] != nums[j-1]){
                 nums[j] = nums[i];
                 j++;
             }
