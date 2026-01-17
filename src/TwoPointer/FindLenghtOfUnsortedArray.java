347da640b53ffa776eadc5e1c09eee84d05404f8
SaishWadnere <saishwadnere3@gmail.com>
2026-01-18 00:41:38 +0530

    Fixed Length of Unsorted array

--- src/TwoPointer/FindLenghtOfUnsortedArray.java
+++ src/TwoPointer/FindLenghtOfUnsortedArray.java
@@ -13,7 +13,7 @@ public class FindLenghtOfUnsortedArray {
         int l =-1;
         int r = -1;
         int n= nums.length;
-        for(int i =0 ; i < n-1 ; i++){
+        for(int i =0 ; i < n ; i++){
             max= Math.max(nums[i],max);
             if( nums[i] < max){
                 r = i;
