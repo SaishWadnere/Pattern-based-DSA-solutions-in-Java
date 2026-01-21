691c76214562d15696fa2d81aec6089d3522f50e
SaishWadnere <saishwadnere3@gmail.com>
2026-01-22 00:13:03 +0530

    Optimized BackSpace String Compare question

--- src/TwoPointer/BackSpaceStringCompare.java
+++ src/TwoPointer/BackSpaceStringCompare.java
@@ -10,41 +10,45 @@ Space Complexity: O(1)
 
 public class BackSpaceStringCompare {
     public boolean backspaceCompare(String s, String t) {
-        int count = 0;
-        int m = s.length();
-        int n = t.length();
-        String res1 = "";
-        String res2 = "";
+        int i = s.length()-1 , j =t.length()-1;
+        int skipS=0, skipT =0;
 
-        for (int i = m - 1; i >= 0; i--) {
-            if (s.charAt(i) == '#') {
-                count++;
-                continue;
+        while(i >= 0 || j >= 0){
+
+            while(i >= 0){
+                if(s.charAt(i) == '#'){
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                }else{
+                    break;
+                }
             }
 
-            if (count != 0) {
-                count--;
-            } else {
-                res1 = s.charAt(i) + res1;
+            while(j >= 0){
+                if(t.charAt(j) == '#'){
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                }else{
+                    break;
+                }
             }
+
+            if(i>=0 && j>=0){
+                if(s.charAt(i) != t.charAt(j)) return false;
+            } else if (i>=0 || j>=0) {
+                return false;
+            }
+
+            i--;
+            j--;
         }
-        count = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            if (t.charAt(i) == '#') {
-                count++;
-                continue;
-            }
-
-            if (count != 0) {
-                count--;
-            } else {
-                res2 = t.charAt(i) + res2;
-            }
-        }
-
-
-        return res1.equals(res2);
+        return true;
     }
-
-
 }
+
